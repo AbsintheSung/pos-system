07e34eb20ee0cff1b0dec7dd01447e7f9d984813
AbsintheSung <h90243768@gmail.com>
2024-10-05 22:29:08 +0800

    feat:配置主題

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,61 @@
 import "@/assets/styles/index.scss"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura' //目前PrimeVue提供的預設主題之一
+import Lara from '@primevue/themes/lara' //目前PrimeVue提供的預設主題之一
+import Nora from '@primevue/themes/nora' //目前PrimeVue提供的預設主題之一
+import { definePreset } from '@primevue/themes'; //配置 客製化設定
+
+const MyCustom = definePreset(Aura, {
+    semantic: {
+        primary: {
+            //引入主題色 ( 要修改其他在元件內修改 )
+            50: '#fffbec',
+            100: '#fff6d4',
+            200: '#ffe9a7',
+            300: '#ffd870',
+            400: '#ffbb36',
+            500: '#ffa40f',
+            600: '#fa8c05',
+            700: '#c86706',
+            800: '#9e500e',
+            900: '#7f420f',
+            950: '#452005'
+        },
+    }
+});
 
 const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: MyCustom,
+        options: {
+            prefix: 'p', // 前綴，預設為 p
+            darkModeSelector: '.my-app-dark',
+            cssLayer: { // tailwindCSS 及 primeVue 載入順序設定
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            },
+        },
+    },
+    pt: {
+        global: {
+            // 全域css設定
+            // css: `
+            // .p-datepicker-today > .custom-test {
+            //     background-color: red;
+            // }
+            // .p-dialog-close-button:hover{
+            //     background-color: red;
+            // }
+            // `
+        },
+
+    }
+});
 
 app.use(createPinia())
 app.use(router)
