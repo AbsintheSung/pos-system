0fc1afc9b1531d62b38612ca2e6608e6bc215318
AbsintheSung <h90243768@gmail.com>
2024-10-12 22:27:25 +0800

    feat:添加卡片讀取骨架

--- src/layout/CustomerContainer.vue
+++ src/layout/CustomerContainer.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 <template>
-  <div class="w-full max-w-screen-sm min-h-screen mx-auto bg-primary-50 relative">
+  <div class="w-full max-w-screen-sm min-h-screen mx-auto bg-primary-50 relative flex flex-col">
     <header>
       <slot name="header"></slot>
     </header>
     <main>
       <slot></slot>
     </main>
-    <footer>
+    <footer class="mt-auto">
       <slot name="footer"></slot>
     </footer>
   </div>
