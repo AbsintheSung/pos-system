879d687804fca1995dad2a588cc77ea0c4f6f38a
AbsintheSung <h90243768@gmail.com>
2024-10-14 19:56:30 +0800

    feat:配置反向代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 加載環境變數
+dotenv.config();
+
+// 使用環境變數
+const baseURL = process.env.VITE_APP_API_URL;
+const apiName = process.env.VITE_APP_API_NAME;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,5 +27,19 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      [`/${apiName}`]: {
+        target: baseURL,
+        changeOrigin: true,
+        // 如果有字串需要替換可以加入 rewrite 這個屬性
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      // '/Images': {
+      //   target: baseURL,
+      //   changeOrigin: true,
+      // }
+    },
+  },
 })
