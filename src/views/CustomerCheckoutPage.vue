640644b30ed5b44abeaeb0dc11b8800c06ec5992
AbsintheSung <h90243768@gmail.com>
2024-10-17 00:32:44 +0800

    feat:配置checkout頁面-進度時間軸樣式

--- src/views/CustomerCheckoutPage.vue
+++ src/views/CustomerCheckoutPage.vue
@@ -1,5 +1,37 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import Timeline from 'primevue/timeline'
+import CustomerContainer from '@/layout/CustomerContainer.vue'
+const checkoutTimeList = ref([
+  { title: '訂單資訊', isActive: false, titleNum: 1 },
+  { title: '付款資訊', isActive: false, titleNum: 2 },
+  { title: '確認送出', isActive: false, titleNum: 3 }
+])
+</script>
 <template>
-  <div>測試</div>
+  <CustomerContainer>
+    <template #default>
+      <div class="px-3">
+        <Timeline
+          :value="checkoutTimeList"
+          layout="horizontal"
+          pt:event:class="flex-1 justify-center"
+          pt:eventContent:class="text-center"
+          pt:eventSeparator:class="relative "
+          pt:eventMarker:class="-right-1/2 "
+          pt:eventConnector:class="absolute -right-1/2"
+        >
+          <template #marker="slotProps">
+            <div class="rounded-full w-4 h-4 bg-primary-600 flex justify-center items-center text-[12px] mx-auto z-10">
+              {{ slotProps.item.titleNum }}
+            </div>
+          </template>
+          <template #content="slotProps">
+            {{ slotProps.item.title }}
+          </template>
+        </Timeline>
+      </div>
+    </template>
+  </CustomerContainer>
 </template>
-<style scoped></style>
+<style scoped lang="scss"></style>
