f822aa4c0228be97dc2460f69889d0edd1f8b9cc
AbsintheSung <h90243768@gmail.com>
2024-10-21 21:03:25 +0800

    feat:配置-確認送出部分樣式

--- src/views/customer-page/CustomerCheckoutPage.vue
+++ src/views/customer-page/CustomerCheckoutPage.vue
@@ -131,7 +131,7 @@ const handleReceipt = (val) => {
       </div>
     </template>
 
-    <!-- <template #default>
+    <template v-if="orderNowStatus === OrderStages.ORDER_INFO">
       <div class="p-3 flex flex-col gap-y-2">
         <h2 class="font-bold text-xl">取餐資訊</h2>
         <div class="flex justify-start">
@@ -161,8 +161,8 @@ const handleReceipt = (val) => {
       <RewardsProgram />
       <CheckoutOrderContent />
       <SpecialNeeds />
-    </template> -->
-    <template #default>
+    </template>
+    <template v-if="orderNowStatus === OrderStages.PAYMENT_INFO">
       <div class="p-3 flex flex-col gap-y-2">
         <h2 class="font-bold text-xl">付款資訊</h2>
         <SelectButton
@@ -208,6 +208,30 @@ const handleReceipt = (val) => {
             </div>
           </template>
         </Card>
+      </div>
+    </template>
+    <template v-if="orderNowStatus === OrderStages.CONFIRMATION">
+      <div class="p-3 flex flex-col gap-y-2">
+        <h2 class="font-bold text-xl text-center">
+          <i class="pi pi-check-circle" style="font-size: 1rem"></i>
+          訂單送出
+        </h2>
+        <div class="flex items-center gap-x-6">
+          <div class="flex flex-col gap-y-2">
+            <h3 class="font-medium text-center">取餐編號</h3>
+            <p class="py-3 px-2 rounded-md border-2 bg-neutral-50 border-neutral-950">
+              <span class="font-bold text-3xl">A99</span>
+            </p>
+          </div>
+          <ul class="font-medium flex flex-col gap-y-3 flex-grow">
+            <li>取餐方式 : 內用</li>
+            <li>點餐時間 : 7/24 (三) 11:00</li>
+            <li>付款狀態 : 已結帳</li>
+          </ul>
+        </div>
+      </div>
+      <div class="p-3 flex flex-col gap-y-2">
+        <h2>訂單明細</h2>
       </div>
     </template>
 
