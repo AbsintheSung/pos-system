c8f6277ab8e5d6119a73600c3270ad46ed52eed2
AbsintheSung <h90243768@gmail.com>
2024-10-22 00:29:31 +0800

    update:調整結構

--- src/views/CarouselPage.vue
+++ src/views/CarouselPage.vue
@@ -4,7 +4,6 @@ import Carousel from 'primevue/carousel'
 import carouselImgOne from '@/assets/images/carousel/carousel-1.jpg'
 import carouselImgTwo from '@/assets/images/carousel/carousel-2.jpg'
 import carouselImgThree from '@/assets/images/carousel/carousel-3.jpg'
-import CustomerContainer from '@/layout/CustomerContainer.vue'
 const carousel = ref(null)
 const homeCarouselImg = ref([
   { imgUrl: carouselImgOne, imgAlt: '輪播圖-藍莓塔' },
@@ -23,13 +22,11 @@ watch(
 )
 </script>
 <template>
-  <CustomerContainer>
-    <template #header>
-      <header class="flex justify-center absolute bottom-14 left-0 right-0 z-50">
-        <RouterLink to="/customer" class="bg-primary-700 border-none rounded-3xl py-2 px-20 text-neutral-50"> 查看菜單 </RouterLink>
-      </header>
-    </template>
-    <template #default>
+  <div class="w-full max-w-screen-sm h-screen relative mx-auto flex flex-col">
+    <header class="flex justify-center absolute bottom-14 left-0 right-0 z-50">
+      <RouterLink to="/customer" class="bg-primary-700 border-none rounded-3xl py-2 px-20 text-neutral-50"> 查看菜單 </RouterLink>
+    </header>
+    <main class="h-full">
       <Carousel
         ref="carousel"
         :value="homeCarouselImg"
@@ -38,48 +35,20 @@ watch(
         :showNavigators="false"
         :autoplayInterval="4000"
         :circular="true"
-        pt:root:class="flex-grow"
-        pt:contentContainer="relative flex-grow"
-        pt:content="flex-grow"
+        pt:root:class="flex-grow h-full"
+        pt:contentContainer="h-full"
+        pt:content="h-full"
         pt:itemList="h-full"
         pt:indicatorList:class="absolute bottom-0 left-0 right-0 gap-x-4"
-        pt:indicatorButton:class="rounded-full  w-3 h-3"
+        pt:indicatorButton:class="rounded-full w-3 h-3"
       >
         <template #item="slotProps">
-          <img :src="slotProps.data.imgUrl" :alt="slotProps.data.imgAlt" class="w-full h-full" />
+          <div class="h-full">
+            <img :src="slotProps.data.imgUrl" :alt="slotProps.data.imgAlt" class="w-full h-full" />
+          </div>
         </template>
       </Carousel>
-    </template>
-  </CustomerContainer>
-  <!-- <main class="w-full max-w-screen-sm min-h-screen mx-auto bg-white px-3 relative">
-    <div class="flex justify-center absolute bottom-14 left-0 right-0 z-50">
-      <RouterLink
-        to="/customer/menu"
-        class="bg-primary-700 border-none rounded-3xl py-2 px-20 text-neutral-50"
-      >
-        查看菜單
-      </RouterLink>
-    </div>
-    <Carousel
-      ref="carousel"
-      :value="homeCarouselImg"
-      :numVisible="1"
-      :numScroll="1"
-      :showNavigators="false"
-      :autoplayInterval="4000"
-      :circular="true"
-      class="-mx-3"
-      pt:indicatorList:class="absolute bottom-0 left-0 right-0 gap-x-4"
-      pt:indicatorButton:class="rounded-full  w-3 h-3"
-    >
-      <template #item="slotProps">
-        <img
-          :src="slotProps.data.imgUrl"
-          :alt="slotProps.data.imgAlt"
-          class="w-full min-h-screen"
-        />
-      </template>
-    </Carousel>
-  </main> -->
+    </main>
+  </div>
 </template>
 <style scoped></style>
